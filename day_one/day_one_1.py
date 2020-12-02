3423d0797233e502c11e2410f2fddf0802f523f3
tjbreshears <49780474+tjbreshears@users.noreply.github.com>
2020-12-01 17:04:46 -1000

    Update day_one_1.py

--- day_one/day_one_1.py
+++ day_one/day_one_1.py
@@ -1,1 +1,9 @@
+with open('day1.csv', 'r') as fil:
+    report = [line.rstrip('\n') for line in fil]
 
+report = [int(x) for x in report]
+
+for i in report:
+    if 2020-i in report:
+        print(i,2020-i,(i*(2020-i)))
+    print(False)
