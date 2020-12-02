59f2ec94919d6fc4620304771192c70a339483f5
tjbreshears <49780474+tjbreshears@users.noreply.github.com>
2020-12-01 18:22:33 -1000

    Added printout of results with labels

--- day_one/day_one_1.py
+++ day_one/day_one_1.py
@@ -1,9 +1,15 @@
-with open('day1.csv', 'r') as fil:
+#Find the two entries that sum to 2020; what do you get if you multiply them together?
+
+with open('day_one.csv', 'r') as fil:
     report = [line.rstrip('\n') for line in fil]
 
 report = [int(x) for x in report]
+solution = []
 
 for i in report:
     if 2020-i in report:
-        print(i,2020-i,(i*(2020-i)))
-    print(False)
+        if i not in solution:
+            solution.append(i)
+
+print("Numbers that add to 2,020: " + "{:,}".format(solution[0]) + " and " + "{:,}".format(solution[1]))
+print("Product: " + "{:,}".format(solution[0]*solution[1]))
