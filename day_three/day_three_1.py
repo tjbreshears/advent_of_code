592d2310c0e816d196cec4cb91c354105c1af854
tjbreshears <49780474+tjbreshears@users.noreply.github.com>
2020-12-03 05:59:36 -1000

    Create day_three_1.py
    Unhappy with line 16. Will try and come back to fix.

---
+++ day_three/day_three_1.py
@@ -0,0 +1,19 @@
+#How many trees would you encounter?
+
+with open('day_three/day_three.csv') as file:
+    path = [line.rstrip('\n') for line in file]
+
+elevation = len(path)
+down = 0
+right = 0
+count = 0
+
+while (down < elevation):
+    if path[down][right] == "#":
+        count += 1
+    down += 1
+    right += 3
+    if right >= 30:
+        right -= (len(path[down]))
+
+print ("Trees Hit: " + str(count))
