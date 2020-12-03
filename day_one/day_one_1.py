a9e99a43d7c1d7db25844d9e08b8f44e18db4063
tjbreshears <49780474+tjbreshears@users.noreply.github.com>
2020-12-02 18:35:57 -1000

    correcting csv file path

--- day_one/day_one_1.py
+++ day_one/day_one_1.py
@@ -1,7 +1,7 @@
 #Find the two entries that sum to 2020; what do you get if you multiply them together?
 
-with open('day_one.csv', 'r') as fil:
-    report = [line.rstrip('\n') for line in fil]
+with open('day_one/day_one.csv', 'r') as file:
+    report = [line.rstrip('\n') for line in file]
 
 report = [int(x) for x in report]
 solution = []
