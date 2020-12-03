ca991d981351cfb61658581e15231282fe5db887
tjbreshears <49780474+tjbreshears@users.noreply.github.com>
2020-12-02 18:45:50 -1000

    Removed duplicate answers and made print out nice

--- day_one/day_one_2.py
+++ day_one/day_one_2.py
@@ -1,10 +1,16 @@
-with open('day1.csv', 'r') as fil:
-    report = [line.rstrip('\n') for line in fil]
+#What is the product of the three entries that sum to 2020?
+
+with open('day_one/day_one.csv', 'r') as file:
+    report = [line.rstrip('\n') for line in file]
 
 report = [int(i) for i in report]
+solution = []
 
 for x in report:
     for y in report:
         if 2020-x-y in report:
-            print(x,y,(x*y*(2020-x-y)))
-    
+           if x not in solution:
+                solution.append(x)
+                
+print("Numbers that add to 2,020: " + "{:,}".format(solution[0]) + ", " + "{:,}".format(solution[1]) + ", and " + "{:,}".format(solution[2]))
+print("Product: " + "{:,}".format(solution[0]*solution[1]*solution[2]))
