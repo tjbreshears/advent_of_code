2e4ab9d265535b97e9f88dc53cbdaddf67a2f9da
tjbreshears <49780474+tjbreshears@users.noreply.github.com>
2020-12-02 18:48:41 -1000

    space edits

--- day_two/day_two_1.py
+++ day_two/day_two_1.py
@@ -14,9 +14,9 @@ vMax = '\-[0-9]{1,}'
 vPassword = '\:\s[a-z]{1,}'
 
 for i in passwords:
-	letter   = re.search(vLetter, i).group(0)
-	min      = int(re.search(vMin, i).group(0))
-	max      = int(re.search(vMax, i).group(0)[1:])
+	letter = re.search(vLetter, i).group(0)
+	min = int(re.search(vMin, i).group(0))
+	max = int(re.search(vMax, i).group(0)[1:])
 	password = re.search(vPassword, i).group(0)[2:]
 
 	if min <= password.count(letter) <= max:
