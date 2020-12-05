7b512406e61c8ed1cca32d29dc2b6c8664fa7333
tjbreshears <49780474+tjbreshears@users.noreply.github.com>
2020-12-05 13:10:51 -1000

    Add files via upload

---
+++ day_05/day_05_1.py
@@ -0,0 +1,35 @@
+#What is the highest seat ID on a boarding pass?
+
+import re
+
+with open('day_05/input_05.csv') as file:
+    data = file.read().strip().split("\n")
+
+IDlist = []
+
+#Searches each letter in a boarding pass seat assignment
+def boarding(ticket):
+    row = range(0, 127)
+    column = range(0, 7)
+
+    for i in ticket:
+        if i == "F":
+            row = row[0:(len(row)) // 2]
+        if i == "B":
+            row = row[(len(row) + 1) // 2:]
+        if i == "L":
+            column = column[0:(len(column)) // 2]
+        if i == "R":
+            column = column[(len(column) + 1) // 2:]
+
+#Calculates seat ID
+    seat_ID = row.stop * 8 + column.stop
+    IDlist.append(seat_ID)
+
+#Looks at every boarding pass
+def allSeats(data):
+    for x in data:
+        boarding(x)
+
+allSeats(data)
+print(f"Highest Seat ID: {max(IDlist)}")
