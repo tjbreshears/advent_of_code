a04424dd893e38062893540c55aad5b5bfba40cc
T.J. Breshears <49780474+tjbreshears@users.noreply.github.com>
2021-12-07 14:23:02 -1000

    2021 - Day 7

---
+++ 2021/day_07/day_07_2.py
@@ -0,0 +1,20 @@
+# How much fuel must they spend to align to that position?
+# https://adventofcode.com/2021/day/7
+
+with open('inputs/2021/day_07.csv', 'r') as f:
+    data =  f.read().strip().split(",")
+    data = [int(x) for x in data]
+
+best = 999999999
+position = 0
+
+while position < max(data):
+    fuel = 0
+    for x in data:
+        n = abs(x-position)
+        fuel += ((n**2)+n)/2
+    if fuel < best:
+        best = fuel
+    position += 1
+
+print(f'Least amount of fuel used:{int(best)}')
